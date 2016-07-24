ce06dc5c56a4943c0f3cbcae053e60ade5276237
Deryou <1171733450@qq.com>
2016-07-24 21:18:52 +0800

    CustomVIew

---
+++ 自定义控件/CustomView/src/com/ccut/customview/view/CustomTitleView.java
@@ -0,0 +1,140 @@
+package com.ccut.customview.view;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
+
+import com.ccut.customview.R;
+
+public class CustomTitleView extends View {
+	private String mTitleText;
+	private int mTitleTextColor;
+	private int mTitleTextSize;
+
+	/**
+	 * 绘制时控制文本绘制的范围
+	 */
+	private Rect mBound;
+	private Paint mPaint;
+
+	/**
+	 * 获得自定义属性
+	 * 
+	 * @param context
+	 * @param attrs
+	 * @param defStyle
+	 */
+	public CustomTitleView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// 获得我们所定义的自定义样式属性
+		TypedArray a = context.getTheme().obtainStyledAttributes(attrs,
+				R.styleable.CustomTitleView, defStyle, 0);
+		int n = a.getIndexCount();
+		for (int i = 0; i < n; i++) {
+			int attr = a.getIndex(i);
+			switch (attr) {
+			case R.styleable.CustomTitleView_titleText:
+				mTitleText = a.getString(attr);
+				break;
+			case R.styleable.CustomTitleView_titleColor:
+				mTitleTextColor = a.getColor(attr, Color.BLACK);
+				break;
+			case R.styleable.CustomTitleView_titleSize:
+				mTitleTextSize = a.getDimensionPixelSize(attr, (int) TypedValue
+						.applyDimension(TypedValue.COMPLEX_UNIT_SP, 16,
+								getResources().getDisplayMetrics()));
+				break;
+			}
+		}
+		a.recycle();
+		
+		//获得绘制文本的宽和高
+		mPaint = new Paint();
+		mPaint.setTextSize(mTitleTextSize);
+		mBound = new Rect();
+		mPaint.getTextBounds(mTitleText, 0, mTitleText.length(), mBound);
+		
+		this.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				mTitleText = randomText();
+				invalidate();
+			}
+		});
+	}
+
+	public CustomTitleView(Context context, AttributeSet attrs) {
+		this(context, attrs, 0);
+	}
+
+	public CustomTitleView(Context context) {
+		this(context, null);
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		mPaint.setColor(Color.YELLOW);
+		canvas.drawRect(0, 0, getMeasuredWidth(), getMeasuredHeight(), mPaint);
+		mPaint.setColor(mTitleTextColor);
+		canvas.drawText(mTitleText, getWidth()/2-mBound.width()/2, getHeight()/2+mBound.height()/2, mPaint);
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		//SpecMode有三种模式：MeasureSpec.ExacTly  MeasureSpec.AT_MOST MeasureSpec.UNSPECIFIED
+		int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+		int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+		int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+		int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+		int width = 0;
+		int height = 0;
+		if(widthMode == MeasureSpec.EXACTLY){
+			width = widthSize;
+		} else {
+			mPaint.setTextSize(mTitleTextSize);
+			mPaint.getTextBounds(mTitleText, 0, mTitleText.length(), mBound);
+			float textHeight = mBound.width();
+			int desired = (int)(getPaddingTop()+textHeight+getPaddingBottom());
+			width = desired;
+		}
+		
+		if(heightMode == MeasureSpec.EXACTLY){
+			height = heightSize;
+		}
+		else {
+			mPaint.setTextSize(mTitleTextSize);
+			mPaint.getTextBounds(mTitleText, 0, mTitleText.length(), mBound);
+			float textHeight = mBound.height();
+			int desired = (int)(getPaddingTop()+textHeight+getPaddingBottom());
+			height = desired;
+		}
+		setMeasuredDimension(width, height);
+	}
+	
+	
+	
+	private String randomText(){
+		Random random = new Random();
+		Set<Integer> set = new HashSet<Integer>();
+		while(set.size()<4){
+			int randomInt = random.nextInt(10);
+			set.add(randomInt);
+		}
+		StringBuffer sb = new StringBuffer();
+		for(Integer i : set){
+			sb.append(""+i);
+		}
+		return sb.toString();
+	}
+}
